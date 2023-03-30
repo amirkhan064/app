6c0978f33277a0aca53f28e5c8a44b91daff35c8
Amir khan <webmasteramir1@gmail.com>
2023-03-30 11:35:09 +0530

    angular standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule, NavComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,31 @@
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/home/home.component';
+import { TodoComponent } from './app/todo/todo.component';
 
-import { AppModule } from './app/app.module';
+const routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
 
+  {
+    path: 'todo',
+    component: TodoComponent,
+  },
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  {
+    path: 'product',
+    loadComponent: () =>
+      import('./app/product-list/product-list.component').then(
+        (d) => d.ProductListComponent
+      ),
+  },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(RouterModule.forRoot(routes))],
+});
